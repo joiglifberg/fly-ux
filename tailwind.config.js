4531542dd8a8903ab8eaa217b18276004e13bde0
Joi Glifberg <joi.glifberg@gmail.com>
2020-12-19 08:12:35 +0100

    Prettify stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,7 @@ module.exports = {
           600: "#1453e5",
           700: "#1145bf",
           800: "#0d3798",
-          900: "#0b2d7c",
+          900: "#0b2d7c"
         },
         gray: {
           50: "#fefefe",
@@ -26,19 +26,19 @@ module.exports = {
           600: "#d6d6d6",
           700: "#b3b3b3",
           800: "#8f8f8f",
-          900: "#757575",
-        },
+          900: "#757575"
+        }
       },
       fontFamily: {
-        sans: ["Roboto", "ui-sans-serif", "system-ui"],
+        sans: ["Roboto", "ui-sans-serif", "system-ui"]
       },
       fontSize: {
-        "3xl": "2rem",
-      },
-    },
+        "3xl": "2rem"
+      }
+    }
   },
   variants: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
-};
+  plugins: []
+}
